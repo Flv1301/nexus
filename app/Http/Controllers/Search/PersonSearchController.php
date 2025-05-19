d32713d55163af352b733370e619decdd6d1dad3
Herbety Thiago <pcpaciberlab@gmail.com>
2025-05-19 16:51:42 -0300

    pesquisas

--- app/Http/Controllers/Search/PersonSearchController.php
+++ app/Http/Controllers/Search/PersonSearchController.php
@@ -142,7 +142,7 @@ class PersonSearchController extends Controller
      * @param Request $request
      * @return Collection
      */
-    public function nexus(Request $request): Collection
+    public function person(Request $request): Collection
     {
         $excludedFields = $request->except('lastname', 'options', '_token');
 
